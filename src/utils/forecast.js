d01dd1467167b6dc6882f18dcea26eaec3f37423
David Barber <david4044@gmail.com>
2019-08-22 12:31:06 -0400

    adding secret keys to two files

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/0eda6037cc0bf299bf3f05962943dd70/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZGF2aWQ0MDQ0IiwiYSI6ImNqejhvMzBmOTBqb2YzY3F4eHZoYTF5aHoifQ.tvaTF2lLK5QqCM-JZQQfTA&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
